b844c8998377e9a7a906c61f016166d14f30f7bf
Andrzej Janaszek <janaszek.andrzej.szkola@gmail.com>
2025-11-27 22:11:22 +0100

    lab8 bariera rw lock

--- lab_8/rw_lock/bariera/bariera.c
+++ lab_8/rw_lock/bariera/bariera.c
@@ -1,31 +1,40 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<pthread.h>
+// #include<stdlib.h>
+// #include<stdio.h>
+// #include<pthread.h>
 
-pthread_mutex_t mutex;
-pthread_cond_t warunek;
-int liczba_wejsc = 0;
-int liczba_watkow = 0;
 
-void bariera_init(int nt){
-    liczba_watkow = nt;
-    liczba_wejsc = 0;
-    pthread_mutex_init(&mutex, NULL);
-    pthread_cond_init(&warunek, NULL);
-};
+// struct Bariera
+// {
+//     pthread_mutex_t mutex;
+//     pthread_cond_t warunek;
+//     int liczba_wejsc;
+//     int liczba_watkow;
+// };
 
-void bariera(void){ 
-    pthread_mutex_lock(&mutex);
 
-    liczba_wejsc++;
+// pthread_mutex_t mutex;
+// pthread_cond_t warunek;
+// int liczba_wejsc = 0;
+// int liczba_watkow = 0;
 
-    // 
-    if(liczba_wejsc == liczba_watkow){
-        liczba_wejsc = 0;
-        pthread_cond_broadcast(&warunek);
-    }else{
-        pthread_cond_wait(&warunek, &mutex);
-    }
+// void bariera_init(int nt, struct Bariera *bariera){
+//     bariera->liczba_watkow = nt;
+//     bariera->liczba_wejsc = 0;
+//     pthread_mutex_init(&bariera->mutex, NULL);
+//     pthread_cond_init(&bariera->warunek, NULL);
+// }
+
+// void bariera(struct Bariera *bariera){ 
+//     pthread_mutex_lock(&bariera->mutex);
+
+//     bariera->liczba_wejsc++;
+
+//     if(bariera->liczba_wejsc == bariera->liczba_watkow){
+//         bariera->liczba_wejsc = 0;
+//         pthread_cond_broadcast(&bariera->warunek);
+//     }else{
+//         pthread_cond_wait(&bariera->warunek, &bariera->mutex);
+//     }
     
-    pthread_mutex_unlock(&mutex);
-};
+//     pthread_mutex_unlock(&bariera->mutex);
+// };
--- lab_8/rw_lock/bariera/main.c
+++ lab_8/rw_lock/bariera/main.c
@@ -2,8 +2,44 @@
 #include<stdio.h>
 #include<pthread.h>
 
-extern void bariera_init(int);
-extern void bariera(void);
+#include<stdlib.h>
+#include<stdio.h>
+#include<pthread.h>
+
+// -bariera
+struct Bariera
+{
+    pthread_mutex_t mutex;
+    pthread_cond_t warunek;
+    int liczba_wejsc;
+    int liczba_watkow;
+};
+
+void bariera_init(int nt, struct Bariera *bariera){
+    bariera->liczba_watkow = nt;
+    bariera->liczba_wejsc = 0;
+    pthread_mutex_init(&bariera->mutex, NULL);
+    pthread_cond_init(&bariera->warunek, NULL);
+}
+
+void bariera(struct Bariera *bariera){ 
+    pthread_mutex_lock(&bariera->mutex);
+
+    bariera->liczba_wejsc++;
+
+    if(bariera->liczba_wejsc == bariera->liczba_watkow){
+        bariera->liczba_wejsc = 0;
+        pthread_cond_broadcast(&bariera->warunek);
+    }else{
+        pthread_cond_wait(&bariera->warunek, &bariera->mutex);
+    }
+    
+    pthread_mutex_unlock(&bariera->mutex);
+};
+
+
+///--------- bariera
+
 
 #define LICZBA_W 4
 //#define LICZBA_W 44
@@ -12,6 +48,8 @@ pthread_t watki[LICZBA_W];
 
 void *cokolwiek( void *arg);
 
+struct Bariera bariera1;
+
 int main( int argc, char *argv[] ){
 
   int i, indeksy[LICZBA_W]; for(i=0;i<LICZBA_W;i++) indeksy[i]=i; 
@@ -19,7 +57,7 @@ int main( int argc, char *argv[] ){
   // use of pthread_once (once_control, init_routine) ?
   // int pthread_once(pthread_once_t *once_control, void (*init_routine)(void));
   // but init_routine has no arguments...
-  bariera_init(LICZBA_W);
+  bariera_init(LICZBA_W, &bariera1);
 
   for(i=0; i<LICZBA_W; i++ ) {
     pthread_create( &watki[i], NULL, cokolwiek, (void *) &indeksy[i] );
@@ -39,19 +77,19 @@ void *cokolwiek( void *arg){
 
   printf("przed bariera 1 - watek %d\n",moj_id);
 
-  bariera();
+  bariera(&bariera1);
 
    printf("przed bariera 2 - watek %d\n",moj_id);
 
-  bariera();
+  bariera(&bariera1);
 
   printf("przed bariera 3 - watek %d\n",moj_id);
 
-  bariera();
+  bariera(&bariera1);
 
   printf("przed bariera 4 - watek %d\n",moj_id);
 
-  bariera();
+  bariera(&bariera1);
 
   printf("po ostatniej barierze - watek %d\n",moj_id);  
 
