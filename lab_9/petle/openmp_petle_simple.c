92da8cf4d7c62a66cf2db96261e21729cb2bf9c3
Andrzej Janaszek <janaszek.andrzej.szkola@gmail.com>
2025-12-05 02:59:36 +0100

    lab9 przygotowanie petle 3.0

---
+++ lab_9/petle/openmp_petle_simple.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <omp.h>
+
+#define WYMIAR 18
+
+// compile:
+// gcc -fopenmp openmp_petle_simple.c -o openmp_petle_simple
+// run
+// OMP_NUM_THREADS=4 ./openmp_petle_simple
+
+int main ()
+{
+  double a[WYMIAR];
+
+  #pragma omp parallel for default(none) shared(a)
+  for(int i=0;i<WYMIAR;i++) a[i]=1.02*i;
+
+  // pętla sekwencyjna
+  double suma=0.0;
+  for(int i=0;i<WYMIAR;i++) {
+      suma += a[i];
+  }
+  
+  printf("Suma wyrazów tablicy: %lf\n", suma);
+
+  // pętla do modyfikacji - docelowo równoległa w OpenMP
+  double suma_parallel=0.0;
+  // ...
+  
+  #pragma omp parallel for default(none) reduction(+:suma_parallel) shared(a) ordered schedule(static, 3)
+  // #pragma omp parallel for default(none) reduction(+:suma_parallel) shared(a) ordered schedule(static)
+  // #pragma omp parallel for default(none) reduction(+:suma_parallel) shared(a) ordered schedule(dynamic, 3)
+  // #pragma omp parallel for default(none) reduction(+:suma_parallel) shared(a) ordered schedule(dynamic)
+  for(int i=0;i<WYMIAR;i++) {
+    int id_w = omp_get_thread_num();
+    // ...
+      suma_parallel += a[i];
+      // ...
+      #pragma omp ordered
+      printf("a[%2d]->W_%1d  \n",i,id_w); 
+  }
+
+  printf("\nSuma wyrazów tablicy równolegle (z klauzulą - ....: %lf\n",
+	 suma_parallel);
+
+}
